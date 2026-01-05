51ee64dbfd7f97b659813810bd7569915ae24169
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 08:35:43 +0500

    Refactor VIP data handling in blurrr.js

--- blurrr.js
+++ blurrr.js
@@ -1,17 +1,9 @@
-let body = $response.body;
-if (body) {
-  try {
-    let obj = JSON.parse(body);
-    obj.data = obj.data || {};
-    obj.data.is_vip = true;
-    obj.data.vip_type = "official";
-    obj.data.vip_expired_at = 2145916800;
-    obj.data.vip_status = 1;
-    obj.data.can_use_vip_assets = true;
-    $done({ body: JSON.stringify(obj) });
-  } catch (e) {
-    $done({});
-  }
-} else {
-  $done({});
-}
+let obj = JSON.parse($response.body);
+obj.data = obj.data || {};
+obj.data.is_vip = true;
+obj.data.vip_status = 1;
+obj.data.vip_type = "official";
+obj.data.vip_expired_at = 4070908800; // 2099 год
+obj.data.can_use_vip_assets = true;
+obj.data.can_use_pro_features = true;
+$done({body: JSON.stringify(obj)});
