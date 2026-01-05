79d4ecbaedfdc3ddf154a74c3ed5c14cefa308ad
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 10:03:18 +0500

    Refactor VIP unlock data structure and package
    Updated VIP unlock logic and current package details.

--- blurrr.js
+++ blurrr.js
@@ -1,9 +1,20 @@
+/*
+ * Blurrr VIP Unlock by Maasea
+ */
 let obj = JSON.parse($response.body);
-obj.data = obj.data || {};
-obj.data.is_vip = true;
-obj.data.vip_status = 1;
-obj.data.vip_type = "official";
-obj.data.vip_expired_at = 4070908800; // 2099 год
-obj.data.can_use_vip_assets = true;
-obj.data.can_use_pro_features = true;
-$done({body: JSON.stringify(obj)});
+
+if (obj.data) {
+    obj.data.is_vip = true;
+    obj.data.vip_type = "official";
+    obj.data.vip_status = 1;
+    obj.data.vip_expired_at = 2145916800; // 2038 год
+    obj.data.can_use_vip_assets = true;
+    
+    // Если есть данные о текущем пакете, меняем его на годовой
+    if (obj.data.current_package) {
+        obj.data.current_package.id = "com.blurrr.app.yearly";
+        obj.data.current_package.name = "Yearly VIP";
+    }
+}
+
+$done({ body: JSON.stringify(obj) });
