18be344880c11f3fbcf1091e5783d109a3d5a2a9
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 07:54:07 +0500

    Refactor VIP status assignment in blurrr.js

--- blurrr.js
+++ blurrr.js
@@ -1,9 +1,8 @@
 let obj = JSON.parse($response.body);
-if ($request.url.indexOf('user/profile') != -1 || $request.url.indexOf('subscribers') != -1) {
-    obj.data = obj.data || {};
+if (obj.data) {
     obj.data.is_vip = true;
     obj.data.vip_type = "official";
-    obj.data.vip_expired_at = 2145916800; 
+    obj.data.vip_expired_at = 2145916800;
     obj.data.vip_status = 1;
     obj.data.can_use_vip_assets = true;
 }
