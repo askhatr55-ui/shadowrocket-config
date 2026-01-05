90a3c587dae9bbf5afbd486230d6985903a0ae70
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 11:07:06 +0500

    Refactor VSCO unlock script for subscription handling

--- vsco.js
+++ vsco.js
@@ -1,29 +1,43 @@
-/*
- * VSCO Unlock by NobyDa (2026)
- */
-const resp = {};
-const obj = JSON.parse(typeof $response != "undefined" && $response.body || null);
+var obj = JSON.parse($response.body);
 
-if (typeof $response == "undefined") {
-    // Очистка кэша (Request)
-    delete $request.headers["x-revenuecat-etag"];
-    delete $request.headers["X-RevenueCat-ETag"];
-    resp.headers = $request.headers;
-    $done(resp);
-} else if (obj && obj.subscriber) {
-    // Выдача подписки (Response)
-    const info = {
-        "expires_date": "2099-01-01T00:00:00Z",
-        "purchase_date": "2023-01-01T00:00:00Z",
-        "ownership_type": "PURCHASED",
-        "store": "app_store"
-    };
-    obj.subscriber.subscriptions["com.circles.fin.premium.yearly"] = info;
-    obj.subscriber.entitlements["membership"] = {
-        ...info,
-        "product_identifier": "com.circles.fin.premium.yearly"
-    };
-    $done({ body: JSON.stringify(obj) });
-} else {
-    $done({});
+obj={
+  "request_date_ms" : 1622445760393,
+  "request_date" : "2021-05-31T07:22:40Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+
+    },
+    "first_seen" : "2021-05-31T07:18:40Z",
+    "original_application_version" : "19615",
+    "other_purchases" : {
+
+    },
+    "management_url" : "itms-apps://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "VSCOANNUAL" : {
+        "is_sandbox" : false,
+        "period_type" : "normal",
+        "billing_issues_detected_at" : null,
+        "unsubscribe_detected_at" : null,
+        "expires_date" : "2090-07-16T12:15:53Z",
+        "grace_period_expires_date" : null,
+        "original_purchase_date" : "2019-07-02T12:15:54Z",
+        "purchase_date" : "2019-07-02T12:15:53Z",
+        "store" : "app_store"
+      }
+    },
+    "entitlements" : {
+      "membership" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2020-11-05T07:03:11Z",
+        "product_identifier" : "VSCOANNUAL",
+        "expires_date" : "2090-12-05T07:03:11Z"
+      }
+    },
+    "original_purchase_date" : "2020-12-28T17:41:34Z",
+    "original_app_user_id" : "139535280",
+    "last_seen" : "2021-05-31T07:18:40Z"
+  }
 }
+
+$done({body: JSON.stringify(obj)});
