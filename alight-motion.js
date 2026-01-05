8ef654879c5c2d5e51ecf8003b7830391310033a
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 08:06:29 +0500

    Remove 'period' field from license response

--- alight-motion.js
+++ alight-motion.js
@@ -1,1 +1,1 @@
-$done({ body: JSON.stringify({"result":"success","licenses":[{"type":"subscription","status":"active","period":"yearly","expire_at":"2038-01-01T00:00:00Z"}]}) });
+$done({ body: JSON.stringify({"result":"success","licenses":[{"type":"subscription","status":"active","expire_at":"2038-01-01T00:00:00Z"}]}) });
