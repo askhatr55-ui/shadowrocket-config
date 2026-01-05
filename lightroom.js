05fb47d4c248049818ca5486aeb6f559912c3ec9
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 07:55:30 +0500

    Fix formatting in lightroom.js response body

--- lightroom.js
+++ lightroom.js
@@ -1,3 +1,3 @@
 let obj = JSON.parse($response.body);
-obj.entitlements = [{"status": "active", "enforcement_method": "f_e", "name": "lightroom_mobile_full_bundle", "requested_at": "2023-01-01T00:00:00Z", "expires_at": "2038-01-01T00:00:00Z"}];
-$done({body: JSON.stringify(obj)});
+obj.entitlements = [{"status":"active","enforcement_method":"f_e","name":"lightroom_mobile_full_bundle","expires_at":"2038-01-01T00:00:00Z"}];
+$done({ body: JSON.stringify(obj) });
