6e987c1a1759f44dfd7b6f06cfd14538f081d00f
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 08:36:20 +0500

    Refactor response structure in alight-motion.js

--- alight-motion.js
+++ alight-motion.js
@@ -1,1 +1,17 @@
-$done({ body: JSON.stringify({"result":"success","licenses":[{"type":"subscription","status":"active","expire_at":"2038-01-01T00:00:00Z"}]}) });
+var objc = {
+  "result": {
+    "result": "success",
+    "msTime": Date.now(),
+    "licenses": [{
+      "benefits": ["RemoveWatermark", "MemberEffects", "ProjectPackageSharing", "FutureMemberFeatures", "AdvancedEasing", "CameraObjects", "LayerParenting", "CloudStorageLowTier"],
+      "type": "subscription",
+      "store": "apple_app_store",
+      "autoRenewing": true,
+      "productId": "alightcreative.motion.1y_t60_1w",
+      "expires": 4070908800000,
+      "valid": true,
+      "linkStatus": "linked-current"
+    }]
+  }
+};
+$done({body: JSON.stringify(objc)});
