8f84932b0e00e2b38a32e8c2097f73e99a45ce4e
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 09:23:33 +0500

    Refactor response body handling in alight-motion.js

--- alight-motion.js
+++ alight-motion.js
@@ -1,17 +1,31 @@
-var objc = {
+let obj = JSON.parse($response.body || "{}");
+
+obj = {
   "result": {
     "result": "success",
     "msTime": Date.now(),
-    "licenses": [{
-      "benefits": ["RemoveWatermark", "MemberEffects", "ProjectPackageSharing", "FutureMemberFeatures", "AdvancedEasing", "CameraObjects", "LayerParenting", "CloudStorageLowTier"],
-      "type": "subscription",
-      "store": "apple_app_store",
-      "autoRenewing": true,
-      "productId": "alightcreative.motion.1y_t60_1w",
-      "expires": 4070908800000,
-      "valid": true,
-      "linkStatus": "linked-current"
-    }]
+    "licenses": [
+      {
+        "benefits": [
+          "RemoveWatermark",
+          "MemberEffects",
+          "ProjectPackageSharing",
+          "FutureMemberFeatures",
+          "AdvancedEasing",
+          "CameraObjects",
+          "LayerParenting",
+          "CloudStorageLowTier"
+        ],
+        "type": "subscription",
+        "store": "apple_app_store",
+        "autoRenewing": true,
+        "productId": "alightcreative.motion.1y_t60_1w",
+        "expires": 4070908800000,
+        "valid": true,
+        "linkStatus": "linked-current"
+      }
+    ]
   }
 };
-$done({body: JSON.stringify(objc)});
+
+$done({ body: JSON.stringify(obj) });
