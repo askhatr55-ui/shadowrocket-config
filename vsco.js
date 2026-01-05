8212223e748ecf269d2e5d19ec917160679caf99
askhatr55-ui <askhatr55@gmail.com>
2026-01-05 09:11:17 +0500

    Remove subscription data handling in vsco.js

--- vsco.js
+++ vsco.js
@@ -1,25 +1,4 @@
-const obj = JSON.parse($response.body || "{}");
-const data = {
-    "expires_date": "2099-01-01T00:00:00Z",
-    "original_purchase_date": "2023-01-01T00:00:00Z",
-    "purchase_date": "2023-01-01T00:00:00Z",
-    "ownership_type": "PURCHASED",
-    "store": "app_store"
-};
-
-if (obj.subscriber) {
-    obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
-    obj.subscriber.entitlements = obj.subscriber.entitlements || {};
-    
-    // Идентификаторы для VSCO
-    const vscoId = "com.circles.fin.premium.yearly";
-    const vscoEnt = "membership";
-
-    obj.subscriber.subscriptions[vscoId] = data;
-    obj.subscriber.entitlements[vscoEnt] = {
-        ...data,
-        "product_identifier": vscoId
-    };
-}
-
-$done({ body: JSON.stringify(obj) });
+const headers = $request.headers;
+delete headers["x-revenuecat-etag"];
+delete headers["X-RevenueCat-ETag"];
+$done({ headers });
